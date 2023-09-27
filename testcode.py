03d73bf6c7b10a1075b90528f9226a1325dfc052
young0428 <dudgb2380@naver.com>
2023-09-27 13:54:09 +0900

    testbranch create

--- testcode.py
+++ testcode.py
@@ -1,5 +1,21 @@
-def aaa():
-    return 1,2,3
+import pyedflib
+edf_path = ""
 
-x,y,z = aaa()
-print(x)
+ictal_state = []
+
+
+ictal_segments = []
+window_size = 2
+current_sec = 0
+for ictal_interval in ictal_state:
+    current_sec = ictal_interval[1] # [0] == name, [1] == start, [2] == end, [3] == state
+    while(1):
+        if current_sec + window_size > ictal_interval[2]:
+            break
+        # [filename, start, duration] 형태로 저장
+        ictal_segments.append( [edf_path+'/'+ictal_interval[0]+'.edf', current_sec, window_size ] )
+        current_sec += window_size
+
+
+
+
