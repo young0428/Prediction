ab597454ccfc39fc06db19f5a26c6cb106072e27
DESKTOP-53S0IOM\hy105 <dudgb2380@naver.com>
2023-09-29 09:32:07 +0900

    sliding size 고려

--- readDataset.py
+++ readDataset.py
@@ -34,6 +34,7 @@ def Segments2Data(segments):
     # segment[0] = 'filename', segment[1] = 'start', segment[2] = 'duration'
     signal_for_all_segments = []
     name = None
+    read_end = 0
     f = None
     for idx, segment in enumerate(segments):
         if not name == segment[0]:
@@ -46,12 +47,9 @@ def Segments2Data(segments):
         if not skip_start:
             interval_sets = [] # 연속된 구간이면 한번에 읽고 구간 정해진거에 따라 나누기 위해 구간 저장
             read_start = segment[1]
-        read_end = segment[1]+segment[2]
-        if len(interval_sets)==0:
-            interval_sets.append( [0,segments[2]] )
-        else:
-            # [전의 interval의 시작시간 + 전 interval의 duration, duration]
-            interval_sets.append( [interval_sets[len(interval_sets)-1][0]+interval_sets[len(interval_sets)-1][1], segments[2]])
+        if read_end < segment[1] + segment[2]:
+            read_end = segment[1] + segment[2]
+        interval_sets.append([segment[1]-read_start, segment[1]+segment[2]-read_start ])
 
         print(interval_sets)
         if not idx+1 >= len(segments):
@@ -62,11 +60,17 @@ def Segments2Data(segments):
         freq = f.getSampleFrequencies()
         labels = f.getSignalLabels()
         chn_num = len(labels)
-        seg = []
 
-        x = np.linspace(0, 10,int((read_start-read_end)*freq[0]) )
-        x_upsample = np.linspace(0,10,int(256*(read_start-read_end)))
-        
+
+        x = np.linspace(0, 10,int((read_end-read_start)*freq[0]) )
+        x_upsample = np.linspace(0,10,int(256*(read_end-read_start)))
+
+        print(read_start)
+        print(int(freq[0]*(read_end-read_start)))
+        seg = []
+        for i in range(len(interval_sets)):
+            seg.append([])
+
         for i in range(chn_num-1):
             chn_skip = False
             for j in range(i):
@@ -75,17 +79,22 @@ def Segments2Data(segments):
             if chn_skip:
                 continue
             signal = f.readSignal(i,read_start,int(freq[i]*(read_end-read_start)))
+            
             # 256 Hz이하일 경우 256Hz로 interpolation을 이용한 upsampling
             if not freq[i] == 256:
                 signal = np.interp(x_upsample,x, signal)
             
-            seg = [[]]*len(interval_sets)
             for j in range(len(interval_sets)):
-                seg[j].append(signal[interval_sets[j][0] * 256 : (interval_sets[j][0] + interval_sets[j][1]) * 256 ])
+                seg[j].append( list(signal[interval_sets[j][0] * 256 : interval_sets[j][1] * 256 ]) )
                 
+            
+        
+                
+                
+        
         for s in seg:
             signal_for_all_segments.append(s)
-    
+        skip_start = False
     if hasattr(f,'close'):
          f.close()
 
@@ -93,7 +102,8 @@ def Segments2Data(segments):
 
 
 ####    test code    ####
-test = [ ["E:/SNUH_START_END/patient_3/patient_3.edf",0,2],["E:/SNUH_START_END/patient_3/patient_3.edf",2,2] ]
+test = [ ["E:/SNUH_START_END/patient_3/patient_3.edf",0,2],["E:/SNUH_START_END/patient_3/patient_3.edf",2,2],["E:/SNUH_START_END/patient_3/patient_3.edf",3,1],
+        ["E:/SNUH_START_END/patient_3/patient_3.edf",5,2],["E:/SNUH_START_END/patient_3/patient_3.edf",5,1]]
 a = Segments2Data(test)
 #data = LoadDataset('C:/Users/dudgb/Desktop/Prediction/patient_info.csv')
 #segments = Interval2Segments(data['ictal'],2,1)
