4be3b63197ace88b7a9ebb60576b148d2711b92d
DESKTOP-53S0IOM\hy105 <dudgb2380@naver.com>
2023-10-31 03:41:08 +0000

    Paeper base 로 바꿈

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -12,23 +12,18 @@ def FullChannelEncoder(inputs, window_size = 2, dilated = [1,2,4,8,16,32], freq 
 	layers_list = []
 	cnt = 0
 	for df in dilated:
-		x = Conv2D(filters=8, kernel_size = (1,3),padding='same',activation='relu',dilation_rate=(1,df))(inputs)	# (None, 23, 512, 8)
-		x = MaxPooling2D((1,2))(x)	# (None, 23, 256, 8)
-		x = Conv2D(filters=16, kernel_size=(1,3),activation='relu',padding='same')(x)	# (None, 23, 256, 16)
-		x = MaxPooling2D((1,2))(x)	# (None, 23, 128, 16)
-		x = Conv2D(filters=32, kernel_size=(1,3),activation='relu',padding='same')(x)	# (None, 23, 128, 32)
+		x = Conv2D(filters=5, kernel_size = (1,3),padding='same',activation='relu',dilation_rate=(1,df))(inputs)	# (None, 23, 1000, 8)
+		x = MaxPooling2D((1,2))(x)	# (None, 23, 500, 8)
+		x = Conv2D(filters=5, kernel_size=(1,3),activation='relu',padding='same')(x)	# (None, 23, 250, 16)
+		x = MaxPooling2D((1,2))(x)	# (None, 23, 125, 16)
+		x = Conv2D(filters=5, kernel_size=(1,3),activation='relu',padding='same')(x)	# (None, 23, 128, 32)
 		x = MaxPooling2D((1,2))(x)	# (None, 23, 64, 32)
-		x = Conv2D(filters=6-cnt, kernel_size=(channel_num,1),activation='relu', padding='valid')(x)	# (None, 1, 64, 2)
+		x = Conv2D(filters=5, kernel_size=(channel_num,1),activation='relu', padding='valid')(x)	# (None, 1, 64, 2)
 		layers_list.append(x)
 		cnt+=1
 	x = Concatenate(axis=-1)(layers_list)
-	x=  tf.squeeze(x, axis = -3) # (None, 64, 12)
-	# x = Flatten()(x)
-	# x = Dense(encoded_feature_num)(x)
-	# x = BatchNormalization()(x)
-	# x = ReLU()(x)
-	
-	#x = Reshape((int(window_size*freq/pooling_rate),len(dilated)*2))(x)
+	x=  tf.squeeze(x, axis = -3) # (None, 64, 30)
+
 
 	return x
 
@@ -43,15 +38,14 @@ def FullChannelDecoder(inputs, dilated = [1,2,4,8,16,32], pooling_rate = 8, freq
 	cnt = 0
 	sum = 0
 	for i in range(len(dilated)):
-		x = Conv2DTranspose(filters=6-cnt,kernel_size=(21,1),activation='relu',padding='valid')(x_input[:,:,:,sum:sum+(6-cnt)])
+		x = Conv2DTranspose(filters=5,kernel_size=(21,1),activation='relu',padding='valid')(x_input[:,:,:,5*i:5*(i+1)])
 		x = UpSampling2D(size=(1,2))(x)
-		x = Conv2DTranspose(filters=32,kernel_size=(1,3),activation='relu',padding='same')(x)
+		x = Conv2DTranspose(filters=5,kernel_size=(1,3),activation='relu',padding='same')(x)
 		x = UpSampling2D(size=(1,2))(x)
-		x = Conv2DTranspose(filters=16,kernel_size=(1,3),activation='relu',padding='same')(x)
+		x = Conv2DTranspose(filters=5,kernel_size=(1,3),activation='relu',padding='same')(x)
 		x = UpSampling2D(size=(1,2))(x)
-		x = Conv2DTranspose(filters=8,kernel_size=(1,3),padding='same',activation='relu',dilation_rate=dilated[i])(x)
+		x = Conv2DTranspose(filters=5,kernel_size=(1,3),padding='same',activation='relu',dilation_rate=dilated[i])(x)
 		x_list.append(x)
-		sum += 6-cnt
 		cnt+=1
 		
 	x = Concatenate(axis=-1)(x_list)
